7fdf3c87aa88e353b6b24216f4e8dba52b87a8a7
Jarod <jjarod517@gmail.com>
2025-01-29 17:45:46 -0500

    Update

--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -5,9 +5,7 @@ import UserAuthForm from './UserAuthForm'
 import SignInNav from './signinNav'
 
 const SignIn = () => {
-return 
-
-<div className='container mx-auto flex h-screen w-full flex-col justify-center space-y-6 sm:w-[400px]'>
+return <div className='container mx-auto flex h-screen w-full flex-col justify-center space-y-6 sm:w-[400px]'>
     <div className='flex flex-col space-y-2 text-center'>
         <Icons.logo className='container mx-auto flex h-screen w-full flex-col justify-center' />
         <h1 className='text-2xl font-semibold tracking-tight'>Welcome back</h1>
